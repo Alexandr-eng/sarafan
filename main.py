950abaf137ee3023abce2bba00e59aad3738d0b9
Admin <kopylov.a98@mail.ru>
2023-12-01 22:22:41 +0500

    пошло

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
+from imp import reload
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-vitrina = [
-    {"одежда", "обувь", "головные уборы", "термобелье"}
-]
+
 
 @app.get("/")
-def hello():
-    return "hello"
+def hello_index():
+    return {
+        "massage": "hello index"
+
+    }
+
+@app.get("/items")
+def list_items():
+    return [
+        'item1',
+        'item2',
+        'item3',
+    ]
+
+@app.get
+
+
+@app.get("/items/latest/")
+def get_latest_item():
+    return {"item": {"id": 0, "name": "latest"}}
+
+
+@app.get("/items/{items_id}/")
+def get_item_by_id(items_id:int):
+    return {
+       "item": {
+            "id": items_id,
+              
+         },
+    }
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", reload=True)
 
 
