e1406e2366ced6a92cc74579ed94be1d2d8d0106
Admin <kopylov.a98@mail.ru>
2023-12-02 18:12:15 +0500

    lj

--- users/crud.py
+++ users/crud.py
@@ -1,10 +1,6 @@
-
 from users.schemas import CreateUser
 
 
-def create_user(user_in: CreateUser)-> dict:
+def create_user(user_in: CreateUser) -> dict:
     user = user_in.model_dump()
-    return {
-        "success": True,
-        "user": user
-    }
+    return {"success": True, "user": user}
